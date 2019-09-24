b835cdf272b9d712e83cf070277dd7809d2689e0
Conner Krall <conner.krall@gmail.com>
2019-09-24 19:00:57 +0000

    Lesson 7 adding grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,20 @@
 class GramsController < ApplicationController
+  def new
+    @gram = Gram.new
+  end
+
   def index
   end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+
 end
